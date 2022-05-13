b8aa762f98a7d249ba00fa25301c918b039630fe
logerrors <log_e@qq.com>
2022-05-13 22:03:55 +0800

    add xmake problem plugin

--- xmake.lua
+++ xmake.lua
@@ -32,3 +32,36 @@ end
 
 define_target("kth-largest-element-in-an-array")
 define_target("reverse-linked-list")
+
+task("problem")
+
+    set_category("plugin")
+
+    on_run(function()
+        -- 导入参数选项模块
+        import("core.base.option")
+
+        local title = option.get('title')
+        local src_dir = "$(projectdir)/src/"..title
+        if os.exists(src_dir) then 
+            print(src_dir.." has exist!!!")
+        else
+            os.mkdir(src_dir)
+            os.cd(src_dir)
+            os.run("touch solution.hpp")
+            os.run("touch test.cpp")
+            os.run("touch bench.cpp")
+            os.cd('-')
+        end 
+    end)
+
+    set_menu {
+        usage = "xmake problem title" ,
+
+        description = "create leetcode problem template" ,
+
+        options = 
+        {
+            {nil, "title", "v", nil, "the problem title"}
+        }
+    }
